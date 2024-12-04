0a614f44601bf0c70a5a12363cbf88489698a38c
Lenin Sebastian Parra Tapia <leninpt34@uniandes.edu.ec>
2024-12-04 12:00:22 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace GestionTareasPendientes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTarea = new System.Windows.Forms.TextBox();
+            this.btnAgregarTarea = new System.Windows.Forms.Button();
+            this.btnEliminarTarea = new System.Windows.Forms.Button();
+            this.lstTareas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+
+            this.txtTarea.ForeColor = System.Drawing.Color.Gray;
+            this.txtTarea.Location = new System.Drawing.Point(12, 12);
+            this.txtTarea.Name = "txtTarea";
+            this.txtTarea.Size = new System.Drawing.Size(260, 20);
+            this.txtTarea.TabIndex = 0;
+            this.txtTarea.Text = "Escriba su tarea...";
+            this.txtTarea.TextChanged += new System.EventHandler(this.txtTarea_TextChanged);
+            this.txtTarea.Enter += new System.EventHandler(this.txtTarea_Enter);
+            this.txtTarea.Leave += new System.EventHandler(this.txtTarea_Leave);
+
+            this.btnAgregarTarea.BackColor = System.Drawing.Color.Lime;
+            this.btnAgregarTarea.Location = new System.Drawing.Point(83, 38);
+            this.btnAgregarTarea.Name = "btnAgregarTarea";
+            this.btnAgregarTarea.Size = new System.Drawing.Size(119, 32);
+            this.btnAgregarTarea.TabIndex = 1;
+            this.btnAgregarTarea.Text = "Agregar Tarea";
+            this.btnAgregarTarea.UseVisualStyleBackColor = false;
+            this.btnAgregarTarea.Click += new System.EventHandler(this.btnAgregarTarea_Click);
+
+            this.btnEliminarTarea.BackColor = System.Drawing.Color.IndianRed;
+            this.btnEliminarTarea.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEliminarTarea.Location = new System.Drawing.Point(83, 242);
+            this.btnEliminarTarea.Name = "btnEliminarTarea";
+            this.btnEliminarTarea.Size = new System.Drawing.Size(119, 44);
+            this.btnEliminarTarea.TabIndex = 2;
+            this.btnEliminarTarea.Text = "Eliminar Tarea";
+            this.btnEliminarTarea.UseVisualStyleBackColor = false;
+            this.btnEliminarTarea.Click += new System.EventHandler(this.btnEliminarTarea_Click);
+
+            this.lstTareas.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lstTareas.FormattingEnabled = true;
+            this.lstTareas.Location = new System.Drawing.Point(12, 76);
+            this.lstTareas.Name = "lstTareas";
+            this.lstTareas.Size = new System.Drawing.Size(260, 160);
+            this.lstTareas.TabIndex = 3;
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(284, 298);
+            this.Controls.Add(this.lstTareas);
+            this.Controls.Add(this.btnEliminarTarea);
+            this.Controls.Add(this.btnAgregarTarea);
+            this.Controls.Add(this.txtTarea);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion de Tareas Pendientes";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTarea;
+        private System.Windows.Forms.Button btnAgregarTarea;
+        private System.Windows.Forms.Button btnEliminarTarea;
+        private System.Windows.Forms.ListBox lstTareas;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GestionTareasPendientes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtTarea.Text))
+            {
+                txtTarea.Text = "Escriba la Tarea...";
+                txtTarea.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtTarea_Enter(object sender, EventArgs e)
+        {
+            if (txtTarea.Text == "Escriba la Tarea...")
+            {
+                txtTarea.Text = "";
+                txtTarea.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtTarea_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTarea.Text))
+            {
+                txtTarea.Text = "Escriba la Tarea...";
+                txtTarea.ForeColor = Color.Gray;
+            }
+        }
+
+        private void btnAgregarTarea_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTarea.Text) && txtTarea.Text != "Escriba la Tarea...")
+            {
+                lstTareas.Items.Add(txtTarea.Text);
+                txtTarea.Clear();
+                txtTarea.Text = "Escriba la Tarea...";
+                txtTarea.ForeColor = Color.Gray;
+                txtTarea.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese una tarea antes de agregar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEliminarTarea_Click(object sender, EventArgs e)
+        {
+            if (lstTareas.SelectedItem != null)
+            {
+                lstTareas.Items.Remove(lstTareas.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show(" ---Seleccione una tarea para eliminar---", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtTarea_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
